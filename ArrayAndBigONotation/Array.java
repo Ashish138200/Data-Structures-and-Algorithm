8a8f73881a5f1511efaabde439c3e573b11811b6
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-07 21:26:51 +0530

    Basics of array and Big-O

---
+++ ArrayAndBigONotation/Array.java
@@ -0,0 +1,20 @@
+package ArrayAndBigONotation;
+
+public class Array {
+
+    public static void main(String[] args) {
+	    int[] intArray = new int[7]; //size cannot be changed at runtime.
+
+	    intArray[0] = 20;
+	    intArray[1] = 35;
+	    intArray[2] = -15;
+	    intArray[3] = 7;
+	    intArray[4] =55;
+	    intArray[5] = 1;
+	    intArray[6] = -22;
+
+	    for (int i = 0; i < intArray.length; i++) {
+	        System.out.println(intArray[i]);
+        }
+    }
+}
