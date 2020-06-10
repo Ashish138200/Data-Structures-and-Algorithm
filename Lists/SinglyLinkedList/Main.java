5cf86fec1a47b59413f2be958023b931e5aae912
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-10 20:17:55 +0530

    Main class of SinglyLinkedList

---
+++ Lists/SinglyLinkedList/Main.java
@@ -0,0 +1,37 @@
+/*
+(✷‿✷)
+* Each item in the list is called a node
+The first item in the list is the head of the list
+* Last item always points at null
+* Insertion/Deletion at front: O(1)
+ */
+package Lists.SinglyLinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.isEmpty());
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.getSize());
+
+        list.printList();
+
+        list.removeFromFront();
+        System.out.println(list.getSize());
+        list.printList();
+
+    }
+}
