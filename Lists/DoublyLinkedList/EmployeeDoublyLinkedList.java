5bfab9951ee30eba4e0983458a24b0476e63f697
Ashish Chaurasia <53967293+ashish138200@users.noreply.github.com>
2020-06-12 23:17:23 +0530

    Added more comments
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

--- Lists/DoublyLinkedList/EmployeeDoublyLinkedList.java
+++ Lists/DoublyLinkedList/EmployeeDoublyLinkedList.java
@@ -1,5 +1,6 @@
 package Lists.DoublyLinkedList;
 //(✷‿✷)
+// Never forget to increment/decrement the size after adding/removing the element
 public class EmployeeDoublyLinkedList {
 
     private EmployeeNode head;
@@ -8,30 +9,31 @@ public class EmployeeDoublyLinkedList {
 
     public void addToFront(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
-
-        if (head == null) {
+        // we don't have to worry about the new node previous field because it's initialized to null
+        if (head == null) { //for checking if the list is empty
             tail = node;
         }
         else {
-            head.setPrevious(node);
-            node.setNext(head);
+            head.setPrevious(node); //head node pointing to new node
+            node.setNext(head); //new node pointing to head node
         }
 
-        head = node;
+        head = node; //assigning head to the new node
         size++;
     }
 
     public void addToEnd(Employee employee) {
+        // we don't have to worry about the new node next field because it's initialized to null
         EmployeeNode node = new EmployeeNode(employee);
-        if (tail == null) {
+        if (tail == null) { //for checking if the list is empty
             head = node;
         }
         else {
-            tail.setNext(node);
-            node.setPrevious(tail);
+            tail.setNext(node); //tail node pointing to new node
+            node.setPrevious(tail); //new node pointing to tail node
         }
 
-        tail = node;
+        tail = node; //assigning tail to the new node
         size++;
     }
 
@@ -39,40 +41,41 @@ public class EmployeeDoublyLinkedList {
         if (isEmpty()) {
             return null;
         }
+        //head->A<->B<->C->null
+        EmployeeNode removedNode = head; // assigning the head(A) to a variable
 
-        EmployeeNode removedNode = head;
-
-        if (head.getNext() == null) {
+        if (head.getNext() == null) { //if removing the only element from the list
             tail = null;
         }
+
         else {
-            head.getNext().setPrevious(null);
+            head.getNext().setPrevious(null); //assigning the B's previous field to null
         }
 
-        head = head.getNext();
+        head = head.getNext(); //pointing head to B
         size--;
-        removedNode.setNext(null);
-        return removedNode;
+        removedNode.setNext(null); // A pointing to null
+        return removedNode; // A
     }
 
     public EmployeeNode removeFromEnd() {
         if (isEmpty()) {
             return null;
         }
+        //head->A<->B<->C->null
+        EmployeeNode removedNode = tail; // assigning the tail(C) to a variable
 
-        EmployeeNode removedNode = tail;
-
-        if (tail.getPrevious() == null) {
+        if (tail.getPrevious() == null) { //if removing the only element from the list
             head = null;
         }
         else {
-            tail.getPrevious().setNext(null);
+            tail.getPrevious().setNext(null); //assigning the B's next field to null
         }
 
-        tail = tail.getPrevious();
+        tail = tail.getPrevious(); //pointing tail to B
         size--;
-        removedNode.setPrevious(null);
-        return removedNode;
+        removedNode.setPrevious(null); // C pointing to null
+        return removedNode; // C
     }
 
     public int getSize() {
