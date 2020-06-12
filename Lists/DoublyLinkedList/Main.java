1d64bdc71139d8b798f6e695314c655e14a5372e
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-12 22:33:52 +0530

    Main class of DoublyLinkedList

---
+++ Lists/DoublyLinkedList/Main.java
@@ -0,0 +1,41 @@
+/*
+(✷‿✷)
+* Each item in the list has next and previous field
+* The list has a head and a tail
+* Insertion/Deletion from the end of list is O(1) because we've tail reference
+* Insertion/Deletion of node A between nodes B and C: O(1) TC but we've to find the insertion/deletion position
+  first so, this is actually O(n)
+ */
+package Lists.DoublyLinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeDoublyLinkedList list = new EmployeeDoublyLinkedList();
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        list.printList();
+        System.out.println(list.getSize());
+
+        Employee billEnd = new Employee("Bill", "End", 78);
+        list.addToEnd(billEnd);
+        list.printList();
+        System.out.println(list.getSize());
+        list.removeFromFront();
+        list.printList();
+        System.out.println(list.getSize());
+        list.removeFromEnd();
+        list.printList();
+        System.out.println(list.getSize());
+
+    }
+}
