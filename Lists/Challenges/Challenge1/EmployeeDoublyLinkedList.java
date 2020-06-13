6e703ccee5cc86a0fe175efa96a0960bf39ee6c7
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-13 22:38:36 +0530

    EmployeeDLL with a addBefore method
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Lists/Challenges/Challenge1/EmployeeDoublyLinkedList.java
@@ -0,0 +1,130 @@
+package Lists.Challenges.Challenge1;
+import Lists.DoublyLinkedList.Employee;
+import Lists.DoublyLinkedList.EmployeeNode;
+
+public class EmployeeDoublyLinkedList {
+
+    private EmployeeNode head;
+    private EmployeeNode tail;
+    private int size;
+
+    public void addToFront(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+
+        if (head == null) {
+            tail = node;
+        }
+        else {
+            head.setPrevious(node);
+            node.setNext(head);
+        }
+
+        head = node;
+        size++;
+    }
+
+    public void addToEnd(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+        if (tail == null) {
+            head = node;
+        }
+        else {
+            tail.setNext(node);
+            node.setPrevious(tail);
+        }
+
+        tail = node;
+        size++;
+    }
+
+    public boolean addBefore(Employee newEmployee, Employee existingEmployee) {
+
+        // return true if you were able to successfully add the employee
+        // into the list before the existing employee. Return false
+        // if the existing employee doesn't exist in the list
+        if(head == null){
+            return false;
+        }
+        //find the existing employee
+        EmployeeNode current = head;
+        while(current != null && !current.getEmployee().equals(existingEmployee)){
+            current = current.getNext();
+        }
+        if(current==null) return false;
+        //head->A<->C<->D->tail->null
+        //head->A<->(B)<->C<->D->tail->null
+        EmployeeNode newNode = new EmployeeNode(newEmployee); // adding a new Node(B)
+        newNode.setPrevious(current.getPrevious()); //setting the previous field of B with C's previous field i.e. B
+                                                    // will be pointing to A [A<-B]
+        newNode.setNext(current); // B's next field pointing to C [B->C]
+        current.setPrevious(newNode); //C's previous field will point to new Node B [B<-C]
+        if (head == current) {
+            head = newNode;
+        }
+        else {
+            newNode.getPrevious().setNext(newNode); //getting the B's previous field and making it point to B [A->B]
+        }
+        size++;
+        return false;
+    }
+
+    public EmployeeNode removeFromFront() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        EmployeeNode removedNode = head;
+
+        if (head.getNext() == null) {
+            tail = null;
+        }
+        else {
+            head.getNext().setPrevious(null);
+        }
+
+        head = head.getNext();
+        size--;
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public EmployeeNode removeFromEnd() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        EmployeeNode removedNode = tail;
+
+        if (tail.getPrevious() == null) {
+            head = null;
+        }
+        else {
+            tail.getPrevious().setNext(null);
+        }
+
+        tail = tail.getPrevious();
+        size--;
+        removedNode.setPrevious(null);
+        return removedNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void printList() {
+        EmployeeNode current = head;
+        System.out.print("HEAD -> ");
+        while (current != null) {
+            System.out.print(current);
+            System.out.print(" <=> ");
+            current = current.getNext();
+        }
+        System.out.println("null");
+    }
+
+}
