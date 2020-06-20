83837a87a23b0721f75ef4f49ca85a220dabe993
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-20 22:30:53 +0530

    Main class of circular queue
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Queues/CircularQueue/Main.java
@@ -0,0 +1,28 @@
+package Queues.CircularQueue;
+import Stacks.Employee;
+
+//(✷‿✷)
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        ArrayCircularQueue1 queue = new ArrayCircularQueue1(5);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
+        queue.printQueue();
+    }
+}
