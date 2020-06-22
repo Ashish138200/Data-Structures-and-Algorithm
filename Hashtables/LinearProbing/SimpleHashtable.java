4ccd519403c3a984879459d937251acc676b3845
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-22 23:40:08 +0530

    SimpleHashtable with Linear Probing
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Hashtables/LinearProbing/SimpleHashtable.java
@@ -0,0 +1,101 @@
+package Hashtables.LinearProbing;
+import Hashtables.Employee;
+//(✷‿✷)
+/*
+* Open Addressing: If we try to put an employee in the table and we find out that there is already an employee at the
+  slot where we wanted to put the new employee then we look for another position in the array
+* Linear Probing: When we discover that a position for a hashed key value is already occupied then we increment the
+  hashedKey value by 1 then we check the resulting index . It's called LP because each time we increment index we're
+  doing it in linear fashion
+  * Probe: Every increment of the index
+* */
+public class SimpleHashtable {
+
+    private StoredEmployee[] hashtable;
+
+    public SimpleHashtable() {
+        hashtable = new StoredEmployee[10];
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if (occupied(hashedKey)) {
+            int stopIndex = hashedKey;
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            }
+            else {
+                hashedKey++;
+            }
+
+            while (occupied(hashedKey) && hashedKey != stopIndex) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+        }
+
+        if (occupied(hashedKey)) {
+            System.out.println("Sorry, there's already an employee at position " + hashedKey);
+        }
+        else {
+            hashtable[hashedKey] = new StoredEmployee(key, employee);
+        }
+    }
+
+    public Employee get(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+        return hashtable[hashedKey].employee;
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+
+    private int findKey(String key) {
+        int hashedKey = hashKey(key);
+        if (hashtable[hashedKey] != null &&
+                hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
+        }
+
+        int stopIndex = hashedKey;
+        if (hashedKey == hashtable.length - 1) {
+            hashedKey = 0;
+        }
+        else {
+            hashedKey++;
+        }
+
+        while (hashedKey != stopIndex &&
+                hashtable[hashedKey] != null &&
+                !hashtable[hashedKey].key.equals(key)) {
+            hashedKey = (hashedKey + 1) % hashtable.length;
+        }
+
+        if (stopIndex == hashedKey) {
+            return -1;
+        }
+        else {
+            return hashedKey;
+        }
+
+    }
+
+    private boolean occupied(int index) {
+        return hashtable[index] != null;
+    }
+
+    public void printHashtable() {
+        for (int i = 0; i < hashtable.length; i++) {
+            if (hashtable[i] == null) {
+                System.out.println("empty");
+            }
+            else {
+                System.out.println("Position " + i + ": " +hashtable[i].employee);
+            }
+        }
+    }
+
+}
