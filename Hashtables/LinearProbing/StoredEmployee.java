ec5e7f4599d2b6a0b1ed4db499dc7fb57eb32a0f
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-22 23:40:42 +0530

    Stored Employee
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Hashtables/LinearProbing/StoredEmployee.java
@@ -0,0 +1,13 @@
+package Hashtables.LinearProbing;
+import Hashtables.Employee;
+//(✷‿✷)
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
