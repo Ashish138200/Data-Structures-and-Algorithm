c9ffb9ea75e74ab6b9e9b369d5d8b7b2f2f7f03b
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-28 16:16:25 +0530

    Tree class of BST
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Trees/BinarySearchTree/Tree.java
@@ -0,0 +1,19 @@
+package Trees.BinarySearchTree;
+
+public class Tree {
+    private TreeNode root;
+
+    public void insert(int value){
+        if (root ==null){
+            root = new TreeNode(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+    public void traverseInOrder(){
+        if (root != null){
+            root.traverseInOrder();
+        }
+    }
+}
