4d9d422ee5ba6d2e877026906d17f667c555630a
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-30 19:15:25 +0530

    Main Class of challenge 1
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

---
+++ Trees/Challenge1/Main.java
@@ -0,0 +1,25 @@
+package Trees.Challenge1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree intTree = new Tree();
+	    intTree.insert(25);
+	    intTree.insert(20);
+	    intTree.insert(15);
+	    intTree.insert(27);
+	    intTree.insert(30);
+	    intTree.insert(29);
+	    intTree.insert(26);
+	    intTree.insert(22);
+	    intTree.insert(32);
+	    intTree.insert(17);
+
+        intTree.traverseInOrder();
+        System.out.println();
+		intTree.preOrderTraversal();
+		System.out.println();
+		intTree.postOrderTraversal();
+    }
+}
