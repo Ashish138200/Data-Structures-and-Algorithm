7fa3de83cdf24c4fde76e28103a0768b7ef993df
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-06-30 17:01:39 +0530

    Tree class of BST
    Signed-off-by: Ashish138200 <53967293+Ashish138200@users.noreply.github.com>

--- Trees/BinarySearchTree/Tree.java
+++ Trees/BinarySearchTree/Tree.java
@@ -22,6 +22,38 @@ public class Tree {
         }
         return null;
     }
+    public void delete(int value){
+        root = delete(root,value);
+    }
+    private TreeNode delete(TreeNode subtreeRoot,int value){
+        if (subtreeRoot == null){
+            return subtreeRoot;
+        }
+        if (value< subtreeRoot.getData()){
+            // we move to subtree left child and at the end we'll replace the subtree left child with the whatever the
+            //result of replace is
+            subtreeRoot.setLeft(delete(subtreeRoot.getLeft(),value));
+        }
+        else if (value>subtreeRoot.getData()){
+            subtreeRoot.setRight(delete(subtreeRoot.getRight(),value));
+        }
+        else{
+            //Cases 1 and 2 :node to delete has 0 or 1 child(ren)
+            if (subtreeRoot.getLeft() == null){
+                return subtreeRoot.getRight();
+            }
+            else if (subtreeRoot.getRight() == null){
+                return subtreeRoot.getLeft();
+            }
+            //Case 3 : node to delete has 2 children
+            // Replace the value in the subtree node with the smallest value from the right subtree
+            subtreeRoot.setData(subtreeRoot.getRight().min());
+            // Delete the node that has the smallest value in the right subtree
+            subtreeRoot.setRight(delete(subtreeRoot.getRight(),subtreeRoot.getData()));
+        }
+
+        return subtreeRoot;
+    }
     public int min(){
         if (root == null){
             return Integer.MIN_VALUE;
